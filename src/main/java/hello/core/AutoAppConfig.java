58398bc260e2e2fc4a213e324e853ed5f501a30d
oksusutea <chanjukim118@gmail.com>
2021-02-26 10:04:30 +0900

    컴포넌트 스캔 챕터 클론코딩 완료

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class)
+) //스프링 빈을 긁어서 컨테이너에 등록, @Component애노테이션이 들어간 클래스를 찾아 빈으로 등록한다.
+public class AutoAppConfig {
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memoryMemberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+}
